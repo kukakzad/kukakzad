bae12a2f32a6429f70de7f8971e3cf4b46f659e0
Kittitouch Pongthong <ZonlyoneR@Kittitouchs-MacBook-Pro.local>
2016-04-24 13:40:42 +0700

    commit all

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'zonlyoner') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
