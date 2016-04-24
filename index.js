d93a8916cd2565c72102eb82756463370497f1cb
Kittitouch Pongthong <ZonlyoneR@Kittitouchs-MacBook-Pro.local>
2016-04-24 14:16:24 +0700

    edit all

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
     if (event.message && event.message.text) {
-      text = event.message.text;
+      var text = event.message.text;
       // Handle a text message from this sender
+      console.log(text);
     }
   }
   res.sendStatus(200);
